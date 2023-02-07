12cf8b750d80cad0b3d0df182e782befde7e0bb5
ESTHER SITUMA <119051563+EstherSit@users.noreply.github.com>
2023-02-07 10:02:13 -0800

    Update memory.js

--- MEMORY GAME/memory.js
+++ MEMORY GAME/memory.js
@@ -1,89 +1,100 @@
 const cardChoices= [
     {
         name: 'cheeseburger',
-       image: 'cheeseburger.png',
+       img: 'cheeseburger.png',
     },
     {
         name: 'fries',
-        image: 'fries.png',
+        img: 'fries.png',
     },
     {
         name: 'hotdog',
-        image: 'hotdog.png',
+        img: 'hotdog.png',
     },
     {
         name: 'ice-cream',
-      image: 'ice-cream.png'
+      img: 'ice-cream.png'
     },
     {
         name: 'milkshake',
-        image: 'milkshake.png',
+        img: 'milkshake.png',
     },
     {
         name: 'pizza',
-        image: 'pizza.png',
+        img: 'pizza.png',
     },
     {
         name: 'cheeseburger',
-       image: 'cheeseburger.png',
+       img: 'cheeseburger.png',
     },
     {
         name: 'fries',
-        image: 'fries.png',
+        img: 'fries.png',
     },
     {
         name: 'hotdog',
-        image: 'hotdog.png',
+        img: 'hotdog.png',
     },
     {
         name: 'ice-cream',
-      image: 'images/ice-cream.png'
+      img: 'ice-cream.png',
     },
     {
         name: 'milkshake',
-        image: 'images/ice-cream.png',
+        img: 'milkshake.png',
     },
     {
         name: 'pizza',
-        image: 'pizza.png',}
+        img: 'pizza.png',
+    }
     
 ]
-const shuffledCards= cardChoices.sort(() => 0.5 - Math.random())
-const gameBody = document.getElementById('gamebody')
+cardChoices.sort(() => 0.5 - Math.random())
+const gameBody = document.querySelector('#gamebody')
 
 const chosenCards=[]
+const cardsChosenId= []
+const cardsWon= []
 
 
 function cardBoard(){
 for ( let i=0; i < cardChoices.length; i++)
 {
-   const cards= document.createElement('img')
-   cards.setAttribute('src', 'blank.png')
-   cards.setAttribute('data-id', i)
-   cards.addEventListener('click', flipCards)
-   gameBody.appendChild(cards)
+   const card= document.createElement('img')
+   card.setAttribute('src', 'blank.png')
+   card.setAttribute('data-id', i)
+   card.addEventListener('click', flipCards)
+   gameBody.appendChild(card)
 }
 }
 cardBoard()
 
+function checkMatch(){
+const cards= document.querySelectorAll('img')
+if (chosenCards[0] === chosenCards[1]){
+    alert("You got a match!")
+
+    cards[cardsChosenId[0]].setAttribute('src', 'white.png')
+    cards[cardsChosenId[1]].setAttribute('src', 'white.png')
+    cards[cardsChosenId[0]].removeEventListener('click', flipCards)
+    cards[cardsChosenId[1]].removeEventListener('click', flipCards)
+    cardsWon.push(chosenCards)
+}
+}
+
+
 
 
 function flipCards(){
-const cardId= this.getAttriute('data-id')
-chosenCards.push(cardChoices[cardId].name)
-this.setAttribute('src', cardChoices[cardId].image)
-}
-
-
-const results= document.getElementById('result')
-results.textContent=["," ]
-
-
-
-function clickCard(){
-    if ( shuffledCards[0] === shuffledCards[1]){
-return results.textContent= "You won"
-    }else{
-        return results.textContent= "Try Again"
+    const cardId= this.getAttribute('data-id')
+    chosenCards.push(cardChoices[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardChoices[cardId].img)
+    
+    if (chosenCards.length === 2){
+        setTimeout(checkMatch, 200)
     }
-}
+
+
+    }
+
